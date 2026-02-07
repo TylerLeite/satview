a31eb6f3cdc9ebacbac0fd7747465833bff8777e
Tyler Leite <t.leite1995@gmail.com>
2026-02-06 18:09:20 -0800

    fix gitignore for new jsons

--- static/src/components/List/List.tsx
+++ static/src/components/List/List.tsx
@@ -1,38 +1,39 @@
 import './List.css';
 
+import { useMemo, memo } from 'react';
+
 import { useSelectedStore } from '../../stores/selected';
 import { useFilterStore } from '../../stores/filter';
 
 import type { SatRec } from '../Satellites/types';
 import use3leQuery from '../../queries/sat3le';
 
-function ListCard(detail: SatRec) {
-    return(<>
-        <div>
-            {detail.name} ({detail.satnum})
+
+const ListCard = memo(
+    ({name, satnum, search}: SatRec & {search: string}) => 
+        <div className={name.toLocaleLowerCase().includes(search) ? '' : 'hidden'}>
+            {name} ({satnum})
         </div>
-    </>);
-}
+);
 
 export default function List() {
     const { data: satDetails, isLoading, isError } = use3leQuery();
     const selectSatellite = useSelectedStore(s => s.select);
     const search = useFilterStore(s => s.search);
-
+    const search_lower = useMemo(() => search.toLocaleLowerCase(), [search]);
 
     if (isLoading || isError) { return; }
 
-    const cards = satDetails?.
-        filter((e: SatRec) => {
-            return e.name.toLocaleLowerCase().includes(search.toLocaleLowerCase());
-        })
-        .map((e: SatRec, i: number) => { 
-            return <span className="clickable" onClick={() => selectSatellite(i)}><ListCard key={i} {...e} /></span>
-        })
-
     return (<>
         <div className="flex-col list">
-            {cards}
+            {satDetails?.map((e: SatRec, i: number) => (
+                <span
+                    key={e.satnum}
+                    onClick={() => selectSatellite(i)}
+                >
+                    <ListCard search={search_lower} {...e} />
+                </span>
+            ))}
         </div>
     </>);
 }
